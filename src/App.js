35f3a3ac3bbd01e93dd54c884f7d0c907ed722f8
Kyle Holzinger <kyleholzinger@hey.com>
2024-06-06 23:37:09 -0400

    [KH] branding

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import Papa from 'papaparse'
 import { Input } from '@/components/ui/input'
-import { Card, CardHeader, CardContent } from '@/components/ui/card'
+import { Card, CardContent, CardDescription } from '@/components/ui/card'
+import { Menubar, MenubarMenu, MenubarTrigger } from '@/components/ui/menubar'
 import { CrossCircledIcon, CheckCircledIcon } from '@radix-ui/react-icons'
 import { Label } from '@/components/ui/label'
 import { Progress } from '@/components/ui/progress'
@@ -12,16 +13,13 @@ function App() {
   const [loading, setLoading] = useState(false)
   const [progressValue, setProgressValue] = useState(0)
   useEffect(() => {
-    Papa.parse(
-      '/csv-validator/marketability-template.csv',
-      {
-        download: true,
-        complete: (result) => {
-          const headers = result.data[0]
-          setTemplateHeaders(headers)
-        },
-      }
-    )
+    Papa.parse('/csv-validator/marketability-template.csv', {
+      download: true,
+      complete: (result) => {
+        const headers = result.data[0]
+        setTemplateHeaders(headers)
+      },
+    })
   }, [])
 
   const randomize = (approximate) => (0.6 + Math.random() * 0.4) * approximate
@@ -71,52 +69,61 @@ function App() {
   }
 
   return (
-    <div className="App px-20 py-12">
-      <Card className="shadow-2xl">
-        <CardHeader className="text-5xl w-full text-center">
-          CSV Validator
-        </CardHeader>
-        <CardContent className="flex flex-col w-full justify-center items-center pb-16">
-          <div className="max-w-screen-lg w-full mb-12">
-            <div className="pt-8">
-              <Label className="flex flex-col justify-between gap-2">
-                <div className="shrink-0">Test CSV:</div>
-                <Input
-                  disabled={!templateHeaders?.length}
-                  className="w-1/3"
-                  type="file"
-                  accept=".csv"
-                  onChange={handleTestUpload}
-                />
-              </Label>
+    <>
+      <Menubar className="bg-white mx-4 my-3">
+        <MenubarMenu>
+          <MenubarTrigger disabled>CSV Validator</MenubarTrigger>
+        </MenubarMenu>
+      </Menubar>
+      <div className="App px-20 py-12">
+        <Card className="shadow-2xl items-center">
+          <CardDescription className="text-lg pl-24 pt-8">
+            Hello! Please upload a CSV file to validate it against the template.
+            Once you upload the file, we will check if all the columns are
+            present.
+          </CardDescription>
+          <CardContent className="flex flex-col w-full justify-center items-center pb-16">
+            <div className="max-w-screen-lg w-full mb-12">
+              <div className="pt-8">
+                <Label className="flex flex-col justify-between gap-2">
+                  <div className="shrink-0">Test CSV:</div>
+                  <Input
+                    disabled={!templateHeaders?.length}
+                    className="w-1/3"
+                    type="file"
+                    accept=".csv"
+                    onChange={handleTestUpload}
+                  />
+                </Label>
+              </div>
             </div>
-          </div>
-          {loading ? (
-            <Progress className="w-1/2" value={progressValue} />
-          ) : (
-            <div className="max-w-screen-lg w-full">
-              {validationResult && (
-                <>
-                  <div className="text-xl flex pb-6">Validation Result</div>
-                  {validationResult.success ? (
-                    <span className="flex gap-2 items-center">
-                      <CheckCircledIcon className="text-green-500 size-6" />
-                      All columns are present!
-                    </span>
-                  ) : (
-                    <span className="flex gap-2 items-center">
-                      <CrossCircledIcon className="text-red-500 size-6" />
-                      Missing columns:{' '}
-                      {validationResult.missingHeaders.join(', ')}
-                    </span>
-                  )}
-                </>
-              )}
-            </div>
-          )}
-        </CardContent>
-      </Card>
-    </div>
+            {loading ? (
+              <Progress className="w-1/2" value={progressValue} />
+            ) : (
+              <div className="max-w-screen-lg w-full">
+                {validationResult && (
+                  <>
+                    <div className="text-xl flex pb-6">Validation Result</div>
+                    {validationResult.success ? (
+                      <span className="flex gap-2 items-center">
+                        <CheckCircledIcon className="text-green-500 size-6" />
+                        All columns are present!
+                      </span>
+                    ) : (
+                      <span className="flex gap-2 items-center">
+                        <CrossCircledIcon className="text-red-500 size-6" />
+                        Missing columns:{' '}
+                        {validationResult.missingHeaders.join(', ')}
+                      </span>
+                    )}
+                  </>
+                )}
+              </div>
+            )}
+          </CardContent>
+        </Card>
+      </div>
+    </>
   )
 }
 
